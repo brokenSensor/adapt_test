d26c14e1161f83f45e2e3e237f03998b25a1ea38
brokenSensor <jonimen95@yandex.ru>
2021-09-17 09:44:10 +0800

    Deploy fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useGetDataQuery } from './api/apiSlice'
 import Card from './components/Card'
+import { response } from './entities.json'
 function App() {
-	const { data } = useGetDataQuery()
+	let { data, isError, isLoading } = useGetDataQuery()
+	if (!isLoading && isError) data = response
 	return (
 		<div className='App'>
 			{data && data.map((item, index) => <Card key={index} item={item} />)}
